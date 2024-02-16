76f898de6316f19a59092bb146aa607e0a42609b
thomasdegroot <thomas.de.groot2@rws.nl>
2024-02-16 14:46:19 +0100

    added commented out code for showing nested RecordPatterns

--- src/main/java/com/dithomas/jworkshop/amber/RecordPatterns.java
+++ src/main/java/com/dithomas/jworkshop/amber/RecordPatterns.java
@@ -4,10 +4,12 @@ package com.dithomas.jworkshop.amber;
 public class RecordPatterns {
 
     public String determineClubType(Object club){
-        if (club instanceof FootballClub(var clubName, var ranking, ClubManager cm)){
+//        The RecordPattern accepts forms of nesting, but in this my personal preference is not using it to recognize certain structure
+//        if (club instanceof FootballClub(var clubName, var ranking, ClubManager(var name, var age))){
+        if (club instanceof FootballClub(var clubName, var ranking, var clubManager)){
             System.out.println("Club name: " + clubName);
             System.out.println("Ranking: " + ranking);
-            System.out.println("Club Manager: " + cm.name());
+            System.out.println("Club Manager: " + clubManager.name());
             return "FOOTBALLCLUB";
         }else if (club instanceof FanClub(var clubName, var idol)){
             System.out.println("Club name: " + clubName);
