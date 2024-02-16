51c544e454ce0faa062abe46e5d11bcb0492ab3c
thomasdegroot <thomas.de.groot2@rws.nl>
2024-02-16 15:39:04 +0100

    created old version + added comments

--- src/main/java/com/dithomas/jworkshop/amber/RecordPatterns.java
+++ src/main/java/com/dithomas/jworkshop/amber/RecordPatterns.java
@@ -1,24 +1,53 @@
 package com.dithomas.jworkshop.amber;
 
 
+import java.util.regex.Pattern;
+
+/**
+ * RecordPatterns is released since Java 21,
+ * with it, it's possible to write cleaner code where you can access the fields directly
+ * This feature is build upon Java 16 released Pattern Matching for instanceof
+ */
 public class RecordPatterns {
 
-    public String determineClubType(Object club){
-        if (club instanceof FootballClub(var clubName, var ranking, ClubManager cm)){
+    public String determineClubType(Object club) {
+        /**
+         * FIXME: This instanceof can be rewritten so that record values are deconstructed
+         * Hint: FootballClub itself can be written to a Pattern, which can be then accessed directly
+         */
+        //(Hints:
+        if (club instanceof FootballClub) {
+            //Prior to Java 16 this ugly cast is always needed
+            var fc = (FootballClub)club;
+            //Since Java 21 it is possible to access these fields as deconstructed values
+            var clubName = fc.name();
+            int ranking = fc.ranking();
+            ClubManager clubManager = fc.clubManager();
             System.out.println("Club name: " + clubName);
             System.out.println("Ranking: " + ranking);
-            System.out.println("Club Manager: " + cm.name());
+            System.out.println("Club Manager: " + clubManager.name());
             return "FOOTBALLCLUB";
-        }else if (club instanceof FanClub(var clubName, var idol)){
+        } else if (club instanceof FanClub fc) {
+            var clubName = fc.name();
+            var idol = fc.idol();
             System.out.println("Club name: " + clubName);
             System.out.println("Idolizing person: " + idol);
             return "FANCLUB";
-        }else {
+        } else {
             return null;
         }
+
     }
+
+
+
+
+
 }
 
-record FootballClub(String name, int ranking, ClubManager clubManager) { }
-record FanClub(String name, String idol){}
-record ClubManager(String name, int age){}
+record FootballClub(String name, int ranking, ClubManager clubManager) {
+}
+record FanClub(String name, String idol) {
+}
+record ClubManager(String name, int age) {
+}
